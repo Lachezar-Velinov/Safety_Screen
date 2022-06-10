ce4b1d4559ca5041a97bafc640d5ee0a638f269d
Yassen Efremov <48584741+YassenEfremov@users.noreply.github.com>
2022-06-10 13:02:30 +0300

    Fix bug on checkout when the people count is maxed

--- Safety_Screen.ino
+++ Safety_Screen.ino
@@ -17,15 +17,12 @@ const int rs = 7, en = 6, d4 = 5, d5 = 4, d6 = 3, d7 = 2;
 LiquidCrystal lcd(rs, en, d4, d5, d6, d7);
 
 // Initialize the RFID readers
-#define RST_PIN 9
-#define OUTSIDE_PIN 10
-#define INSIDE_PIN 8
+const int rst_pin = 9;
+const int outside_pin = 10;
+const int inside_pin = 8;
+MFRC522 mfrc522_outside(outside_pin, rst_pin);
+MFRC522 mfrc522_inside(inside_pin, rst_pin);
 
-// Create MFRC522 instances
-MFRC522 mfrc522_outside(OUTSIDE_PIN, RST_PIN);
-MFRC522 mfrc522_inside(INSIDE_PIN, RST_PIN);
-
-// Create Adafruit_BMP280 instance
 Adafruit_BMP280 bmp;
 
 
@@ -43,12 +40,11 @@ void setup() {
 
     Serial.begin(9600);
     pinMode(A0, INPUT);
-    pinMode(A1, INPUT);
     
+    SPI.begin();      // Initiate  SPI bus
+
     // Set up the LCD's number of columns and rows
     lcd.begin(16, 2);
-
-    SPI.begin();      // Initiate  SPI bus
     
     // Initialize MFRC522
     mfrc522_outside.PCD_Init();
@@ -58,11 +54,11 @@ void setup() {
 
     // Initialize BMP280
     bmp.begin(0x76);
-    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     /* Operating Mode. */
-                    Adafruit_BMP280::SAMPLING_X2,     /* Temp. oversampling */
-                    Adafruit_BMP280::SAMPLING_X16,    /* Pressure oversampling */
-                    Adafruit_BMP280::FILTER_X16,      /* Filtering. */
-                    Adafruit_BMP280::STANDBY_MS_500); /* Standby time. */
+    bmp.setSampling(Adafruit_BMP280::MODE_NORMAL,     // Operating Mode.
+                    Adafruit_BMP280::SAMPLING_X2,     // Temp. oversampling
+                    Adafruit_BMP280::SAMPLING_X16,    // Pressure oversampling
+                    Adafruit_BMP280::FILTER_X16,      // Filtering.
+                    Adafruit_BMP280::STANDBY_MS_500); // Standby time.
 
     // Set up the LCD display layout
     // These are messages that only need to be printed once
@@ -89,21 +85,21 @@ void loop() {
     lcd.setCursor(0, 1);
     lcd.print(bmp.readTemperature(), 1);
     lcd.setCursor(12, 1);
-    lcd.print(analogRead(A1));
+    lcd.print(analogRead(A0));
     lcd.print("    ");  // to clear the previous value
     lcd.setCursor(14, 0);
     lcd.print(people_count);
     lcd.print("   ");    // to clear the previous value
 
 
-    // Check how many cards have been read
-    if(people_count == 30) {
-        return;     // No more cards can be read
-    }
-
     if (mfrc522_outside.PICC_IsNewCardPresent()) {
         // Found chip/card on the outside reader
 
+        // Check how many cards have been read
+        if(people_count == 30) {
+            return;     // No more cards can be read
+        }
+     
         // Select the card/chip
         if (!mfrc522_outside.PICC_ReadCardSerial()) {
             return;
